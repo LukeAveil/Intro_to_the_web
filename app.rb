6200585d322a9c2f5078f25f88010b06b7fbdfaf
Luke <lukeaveil@yahoo.com>
2016-06-07 12:20:24 +0100

    changed name instance variable to a params

--- app.rb
+++ app.rb
@@ -12,9 +12,15 @@ get '/supersecret' do
   "( . Y . )<br>( . Y . )"
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
   erb(:index) 
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  erb(:index)
 end
 
 set :session_secret, 'super secret'
